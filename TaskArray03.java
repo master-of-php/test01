b66b2a61b3074e31d66756dbf6fd13d1f0a77aec
timofiy <timofiy@inuits.eu>
2022-07-30 14:07:40 +0300

    Revert "add comment to class"
    This reverts commit a5ea5e06af61d946b6d9f3eb762808280f891f11.
    
    # Conflicts:
    #	TaskArray03.java

--- TaskArray03.java
+++ TaskArray03.java
@@ -1,6 +1,7 @@
 package test01;
 
 public class TaskArray03 {
+    //need add main method
     public static void main(String[] args) {
         String[][] xyArray= new String[8][8];
         for (int i = 0; i < xyArray.length; i++) {
